c2929c24daf0f2bf8d8d43506ec3b6446b339b5c
kevinagustian <kevinagustian218@gmail.com>
2025-02-20 10:55:15 +0700

    Initial commit

---
+++ app/src/main/java/com/smkth/app_belajar_vcs_kevin/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.smkth.app_belajar_vcs_kevin
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.smkth.app_belajar_vcs_kevin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btn1.setOnClickListener {
+            Toast.makeText(this, "KONTOL", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
